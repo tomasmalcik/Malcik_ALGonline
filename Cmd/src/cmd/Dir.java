d117bc8e9a81563e1179e61e19116b78314048cd
tomasmalcik <tomas.malcik@tul.cz>
2020-05-27 22:02:09 +0200

    Add CMD

--- Cmd/src/cmd/Dir.java
+++ Cmd/src/cmd/Dir.java
@@ -6,8 +6,6 @@
 package cmd;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Date;
 
 /**
@@ -19,34 +17,26 @@ public class Dir extends Command{
     @Override
     public String execute(File actualDir) {
         File[] files;
-        if(params.length == 1){
+        if (params.length == 1) {
             files = actualDir.listFiles();
             return dirToString(files);
-        }
-        else if (params.length == 2  &&  params[1].equals("-o")) {
-            System.out.println("podminka");
-            files = actualDir.listFiles();
-            Arrays.sort(files,Comparator.comparingLong(File::lastModified));
+        }else if(params.length == 2 && params[1] == "-o"){
             
-//            (files).toString();
-           return dirToString(files);
         }
-        return null;
+        return "invalid parameter";
     }
 
     private String dirToString(File[] files) {
         StringBuilder sb = new StringBuilder("");
         for (File file : files) {
-            if(file.isDirectory()){
-                sb.append(String.format("%s%n",file.getName()));
+            if (file.isDirectory()) {
+                sb.append(String.format("%s\n", file.getName()));
             }else{
-                sb.append(String.format("%-20s%6d", file.getName(), file.length())); 
-                sb.append(new Date(file.lastModified())).append("\n");
+                sb.append(String.format("%-20s%6d", file.getName(), file.length()));
+                sb.append(new Date(file.lastModified()) + "\n");
             }
-            
         }
-        String st = sb.toString();
-        return st;
+        return sb.toString();
     }
     
 }
