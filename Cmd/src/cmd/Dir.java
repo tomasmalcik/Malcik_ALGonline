1443aea17537d9c9f3a9941e2f54e5e7358bebe4
tomasmalcik <tomas.malcik@tul.cz>
2020-06-06 16:06:20 +0200

    add dir -r

--- Cmd/src/cmd/Dir.java
+++ Cmd/src/cmd/Dir.java
@@ -1,16 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package cmd;
 
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
-import java.lang.*;
 
 /**
  *
- * @author janvit
+ * @author Tomáš Malčík
  */
-public class Dir extends Command {
+public class Dir extends Command{
 
     @Override
     public String execute(File actualDir) {
@@ -19,37 +23,46 @@ public class Dir extends Command {
             files = actualDir.listFiles();
             return dirToString(files);
         }
-        if (Arrays.asList(params).contains("-r")){
-            StringBuilder dirRecursive = new StringBuilder();
+        else if (params.length == 2  &&  params[1].equals("-o")) {
             files = actualDir.listFiles();
-            for (File file : files) {
-                if (file.isDirectory()){
-//                    dirRecursive.append("./");
-                    dirRecursive.append(dirToString(new File[]{file})).insert(dirRecursive.length()-2,"/");
-                    dirRecursive.append(execute(file));
-                }
-                else{
-                    dirRecursive.append("└ ");
-                    dirRecursive.append(dirToString(new File[]{file}));
-                }
-            }
-            return dirRecursive.toString();
+            //Arrays.sort(files,Comparator.comparingLong(File::lastModified));
+            
+//            (files).toString();
+           return dirToString(files);
+        }else if (params.length == 2 && params[1].equals("-r")) {
+            return recursion(actualDir,"-");
         }
         return null;
     }
 
+    private String recursion(File fil, String level) {
+        StringBuilder recursionString = new StringBuilder("");
+        File[] files = fil.listFiles();
+        for(File f : files) {
+            if(f.isDirectory()) {
+                recursionString.append(String.format(level+"%s%n"," "+f.getName()));
+                File curr = new File(f.getParent() + File.separator + f.getName());
+                recursionString.append(recursion(curr,level+"-"));
+            }else {
+                recursionString.append(String.format(level+"%s%n", " "+f.getName(), " "+f.length()));
+            }
+        }
+        return recursionString.toString();
+    }
+    
     private String dirToString(File[] files) {
         StringBuilder sb = new StringBuilder("");
         for (File file : files) {
             if(file.isDirectory()){
-                sb.append(String.format("%s%n", file.getName()));
-            } else {
-                sb.append(String.format("%-20s%6d ", file.getName(), file.length()));
+                sb.append(String.format("%s%n",file.getName()));
+            }else{
+                sb.append(String.format("%-20s%6d", file.getName(), file.length())); 
                 sb.append(new Date(file.lastModified())).append("\n");
             }
+            
         }
-        return sb.toString();
+        String st = sb.toString();
+        return st;
     }
     
 }
-
